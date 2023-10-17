55e5b46007e60b87a1b4978776c49e660524e4ad
BagusTresna <kadekbagustresna@gmail.com>
2023-10-17 19:49:59 +0800

    memperbaiki koneksi db

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-$db = 'prognet';
-$server = 'localhost';
-$username = 'root';
-$password = '';
+$db = 'db_2205551011';
+$server = 'prognet.localnet';
+$username = '2205551011';
+$password = '2205551011';
 
 $conn = mysqli_connect($server, $username, $password, $db);
 if (!$conn) {
