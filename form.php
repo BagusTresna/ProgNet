50bf0ce94772204a54da39527f366cdc8b36509d
BagusTresna <kadekbagustresna@gmail.com>
2023-10-24 17:59:21 +0800

    push php2

--- form.php
+++ form.php
@@ -5,11 +5,12 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Formulir</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
   <!-- FontAwesome 5 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <link rel="stylesheet" href="{{ asset('css/form.css')}}">
+  <script src="validasi.js"></script>
 </head>
 
 <body>
@@ -21,8 +22,7 @@
           <div class="card-body">
             <h3 class="h2 text-black mb-3" style="text-align: center;">FORMULIR DATA DIRI</h3>
             <hr>
-            <form action="{{ url('/hasil_form')}}" method="post" onsubmit="return validateForm()">
-              @csrf
+            <form action="hasil_form.php" method="post" onsubmit="return validateForm()">
               <div class="mb-3">
                 <label for="nama" class="form-label">Nama</label>
                 <input type="text" id="nama" name="nama" class="form-control" placeholder="Nama" required />
@@ -71,6 +71,20 @@
                   Perempuan
                 </label>
               </div>
+
+              <br>
+
+              <label class="form-label">Kendaraan</label>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="kendaraan[]" value="Motor" id="motor">
+                <label class="form-check-label" for="motor">Motor</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="kendaraan[]" value="Mobil" id="mobil">
+                <label class="form-check-label" for="mobil">Mobil</label>
+              </div>
+
+
               <br />
               <div class="mb-3">
                 <label for="alamat" class="form-label">Alamat</label>
@@ -97,7 +111,7 @@
     integrity = "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin = "anonymous"
   </script>
-  <script src="{{ asset('js/validasi.js')}}"></script>
+  <script src="config/validasi.js"></script>
 </body>
 
 </html>
